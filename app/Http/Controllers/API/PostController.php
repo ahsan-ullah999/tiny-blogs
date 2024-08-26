b1dedd5b42a1cabf491c24f1083ea642b78df046
Ahsan ullah <ahsanullah0216@gmail.com>
2024-08-26 14:38:52 +0600

    isnstall api and Implement CRUD opration in post controller for managing posts

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Request;
 
 class PostController extends Controller
 {
@@ -14,7 +15,12 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::get();
+
+        return response()->json([
+            'message'=>'list of post',
+             'post'=>$posts
+        ], 200);
     }
 
     /**
@@ -22,7 +28,14 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        //
+        $posts=new Post;
+        $posts->title = $request->title;
+        $posts->description = $request->description;
+        $posts ->save();
+        return response()->json([
+            'message'=>'create a new post',
+             'post'=>$posts
+        ], 200);
     }
 
     /**
@@ -30,15 +43,24 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+
+        return response()->json([
+            'message'=>'single post',
+             'post'=>$post
+        ], 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(UpdatePostRequest $request, $id)
     {
-        //
+        $posts = Post::find($id);
+        $posts->update($request->all());
+        return response()->json([
+            'message'=>'post update successful',
+             'post'=>$posts
+        ], 200);
     }
 
     /**
@@ -46,6 +68,21 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        return response()->json([
+            'message'=>'post delete successful',
+             'post'=>$post->delete(),
+        ], 200);
     }
+
+               /**
+     * search the specified resource from storage.
+     * @param str $title
+     * @return \Illuminate\Http\Response
+     *
+     */
+
+     public function search($title)
+     {
+        return Post::where('title', 'like', '%'.$title.'%')->get();
+     }
 }
