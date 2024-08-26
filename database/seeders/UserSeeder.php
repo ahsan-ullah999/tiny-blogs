5718fc6c6265a8f632a6b2788566eeb8a6bb362a
Ahsan ullah <ahsanullah0216@gmail.com>
2024-08-26 13:04:04 +0600

    create UserSeeder to seeding fake data in Database

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+        $users=\App\Models\User::factory(10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+    }
+}
